af480db42b6e492a4badfc5ac4187ac90c4371c7
Leonardo Bloise <leonardocsbloise@gmail.com>
2023-09-13 02:03:29 -0300

    feat: now musics can be played

--- component/Modals/StartMenu.tsx
+++ component/Modals/StartMenu.tsx
@@ -1,24 +1,40 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Modal from "../Modal";
 import ModalButton from "./ModalButton";
 import GameObservable from "@/entity/Game";
+import MuteIcon from "../Icons/MuteIcon";
+import SoundIcon from "../Icons/SoundIcon";
 
 interface Props {
     game: GameObservable
 }
 
-export default function StartMenu({game}: Props) {
+export default function StartMenu({ game }: Props) {
     const [show, setShow] = useState(true);
+    const [playingSound, setPlayingSoud] = useState(false);
 
     const startGame = () => {
         game.start();
         setShow(false);
     }
 
+    const toggleSound = () => {
+        setPlayingSoud(old => !old);
+    }
+
+    useEffect(() => {
+        if(playingSound) {
+            const audio = new Audio('menu.mp3');
+            audio.loop = true;
+            audio.volume = 0.2;
+            audio.play();
+        }
+    }, [playingSound]);
+
     return (
         <>
             {show ? <Modal>
-                <div className={`h-[100%] flex flex-col justify-around border-solid border-white border`}>
+                <div className={`h-[100%] relative flex flex-col justify-around border-solid border-white border`}>
                     <header className='flex flex-col justify-center items-center'>
                         <h1 className='text-white'> Blockeat </h1>
                         <p className='text-white animate-rainbowMy'> Coma e Cresça!</p>
@@ -26,6 +42,9 @@ export default function StartMenu({game}: Props) {
                     <nav role='list' className='flex flex-col items-center'>
                         <ModalButton onClick={startGame} text='PLAY'></ModalButton>
                     </nav>
+                    <div className="absolute bottom-5 right-5 animate-bounce cursor-pointer" onClick={toggleSound}>
+                        {playingSound ? <MuteIcon></MuteIcon>:<SoundIcon></SoundIcon>}
+                    </div>
                 </div>
             </Modal> :
                 null}
