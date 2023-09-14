257bc60b17340c330cbb2710ed9c521cbd6d3427
Leonardo Bloise <leonardocsbloise@gmail.com>
2023-09-14 18:20:36 -0300

    fix: creating game as private to the module

--- contexts/GameContext.tsx
+++ contexts/GameContext.tsx
@@ -1,24 +1,23 @@
 import AudioEngine from "@/entity/AudioEngine";
 import GameObservable from "@/entity/Game";
 import { ReactNode, createContext, useContext } from "react";
-
 interface Game {
     game: GameObservable,
     audioEngine: AudioEngine
 }
-
 const GameContext = createContext<Game>({} as Game);
-
 interface Props {
     children: ReactNode
 }
 
+const game: Game = {
+    game: GameObservable.factory(),
+    audioEngine: new AudioEngine()
+};
+
 export const GameContextProvider = ({children}: Props) => {
     return (
-        <GameContext.Provider value={{
-            game: GameObservable.factory(),
-            audioEngine: new AudioEngine()
-        }}>
+        <GameContext.Provider value={game}>
             {children}
         </GameContext.Provider>
     );
